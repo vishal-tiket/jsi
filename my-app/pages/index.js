3dbd5ff4bbd7fb65eea3d0435757907b32767ebc
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2023-06-08 15:39:23 +0530

    styling update

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
           width: "100%",
           display: "flex",
           alignItems: "center",
-          background: 'black',
+          background: '#0070f3',
           position: 'fixed',
           top: '0',
           left: '0',
