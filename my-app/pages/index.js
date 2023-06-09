d3810580d54eb201e016977f76882917452cc341
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2023-06-09 12:22:18 +0530

    loader update

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -4,15 +4,15 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
     window?.webkit?.messageHandlers?.hideNavbar?.postMessage("");
     window?.native?.hideNavbar();
-    // setTimeout(() => {
-    //   setIsLoading(true);
-    // }, [50]);
+    setTimeout(() => {
+      setIsLoading(true);
+    }, [50]);
 
     function apiSimulation() {
       setTimeout(() => {
